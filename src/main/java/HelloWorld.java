5636317f52830b4a8e97ca3765705617cc429d62
joey <taoyujian2015@163.com>
2017-05-23 14:55:58 +0800

    update for fun1

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * Created by JoeyTao on 2017/5/23.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        System.out.println(s);
+    }
+
+}
